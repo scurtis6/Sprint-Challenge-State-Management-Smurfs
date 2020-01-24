55e2ae7bce37f74328295f05f33dff29ed6c9797
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-24 12:43:51 -0500

    creating a form for new smurf

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchSmurf } from '../actions';
+import SmurfForm from './SmurfForm';
 
 const Smurf = (props) => {
     console.log(props)
---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const SmurfForm = (props) => {
+    const [newSmurf, setNewSmurf] = useState({
+        name: '',
+        age: '',
+        height: ''
+    });
+
+}
