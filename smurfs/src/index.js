2b6b09feffb7598d1beff684b191ad141e36d947
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-24 11:23:20 -0500

    installed dependencies and created store and middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(applyMiddleware(thunk, logger));
+console.log(store.getState());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
