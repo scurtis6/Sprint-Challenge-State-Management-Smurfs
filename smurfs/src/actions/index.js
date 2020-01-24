dfbf620dcd98c1399f2aa1390289a48aeba7f07a
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-24 12:28:52 -0500

    setup action for adding a new smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,10 +4,14 @@ import axios from 'axios';
 export const FETCHING_SMURF = 'FETCHING_SMURF';
 export const FETCHING_SMURF_SUCCESS = 'FETCHING_SMURF_SUCCESS';
 export const FETCHING_SMURF_FAILURE = 'FETCHING_SMURF_FAILURE';
+export const POST_SMURF = 'POST_SMURF';
+export const POST_SMURF_SUCCESS = 'POST_SMURF_SUCCESS';
+export const POST_SMURF_FAILURE = 'POST_SMURF_FAILURE';
 
 export const fetchSmurf = () => dispatch => {
     dispatch({ type: FETCHING_SMURF });
-    axios.get('http://localhost:3333/smurfs')
+    axios
+    .get('http://localhost:3333/smurfs')
     .then(res => {
         console.log(res);
         dispatch({ type: FETCHING_SMURF_SUCCESS, payload: res.data });
@@ -18,3 +22,17 @@ export const fetchSmurf = () => dispatch => {
     });
 
 };
+
+export const addSmurf = (newSmurf) => dispatch => {
+    dispatch({ type: POST_SMURF });
+    axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then (res => {
+        console.log(res);
+        dispatch({ type: POST_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+        console.log(err);
+        dispatch({ type: POST_SMURF_FAILURE, payload: err.response})
+    });
+}
